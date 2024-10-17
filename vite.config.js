0b88a641d7855cdc160612255db82f1a843d6608
injun <jun2000005@naver.com>
2024-10-17 11:29:53 +0900

    vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path, { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/frontProject/dist'),
+      'images': path.resolve(__dirname, '/frontProject/dist/images')
+    }
+  }
 })
