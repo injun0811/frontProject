a7cbea0e692cb911c50076f49e6d660fbedd4400
injun <jun2000005@naver.com>
2024-10-17 11:34:48 +0900

    vite.config.js 롤백 및 이미지 경로 수정_20241017_1134

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path, { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '/frontProject/dist'),
-      'images': path.resolve(__dirname, '/frontProject/dist/images')
-    }
-  }
 })
